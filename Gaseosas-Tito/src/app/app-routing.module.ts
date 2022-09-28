719dfb16648ac98005fa4931127450f4482b9ffb
Manuu1409 <manuracingclub61@gmail.com>
2022-09-28 17:02:33 -0300

    agregando ruteo

--- Gaseosas-Tito/src/app/app-routing.module.ts
+++ Gaseosas-Tito/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoGaseosasTitoComponent } from './contacto-gaseosas-tito/contacto-gaseosas-tito.component';
+import { GaseosaTitoGaseosasComponent } from './gaseosa-tito-gaseosas/gaseosa-tito-gaseosas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "Gaseosas",
+  },
+  {
+    path: "Gaseosas",
+    component: GaseosaTitoGaseosasComponent
+  },
+
+  {
+    path: "Contacto",
+    component: ContactoGaseosasTitoComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
